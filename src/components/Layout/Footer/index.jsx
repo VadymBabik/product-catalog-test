ea639db687f4202e9936cc4a0a94190bb8e925b8
Vadym Babik <vadym.babik@gmail.com>
2021-11-19 20:04:04 +0200

    add footer

---
+++ src/components/Layout/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="page-footer">
+      <div className="footer-copyright center">
+        <div className="container">© 2021 Product catalog</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
